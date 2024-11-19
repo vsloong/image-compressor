9eec9bfe9519571a1817260699138472df1b496b
dragon <wushuailong@psdculture.com>
2024-11-19 14:02:27 +0800

    add take picture feature

--- app/src/main/java/com/vsloong/image/compressor/MainActivity.kt
+++ app/src/main/java/com/vsloong/image/compressor/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.activity.result.PickVisualMediaRequest
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.FlowRow
@@ -27,16 +28,16 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+import androidx.core.content.FileProvider
 import com.vsloong.image.compressor.ui.theme.ImageCompressorTheme
 import com.vsloong.image.compressor.utils.ImageUtil
 import java.io.File
-import java.util.LinkedList
+import java.util.UUID
 
 class MainActivity : ComponentActivity() {
 
     private val TAG = "image-compressor"
-
-    private val queue: LinkedList<File> = LinkedList()
 
     private var inputBitmap = mutableStateOf<Bitmap?>(null)
     private var outputBitmap = mutableStateOf<Bitmap?>(null)
@@ -44,59 +45,77 @@ class MainActivity : ComponentActivity() {
     private val inputFileInfo = mutableStateOf("")
     private val outputFileInfo = mutableStateOf("")
 
-    private var currentFile: File? = null
-
     private var mediaUri: Uri? = null
+    private var pictureUri: Uri? = null
+
+    private fun getOutputDir(): File {
+        return cacheDir.resolve("output")
+    }
+
+    private fun getUriForFile(file: File): Uri {
+        val applicationId = this.packageName
+        return FileProvider.getUriForFile(this, "${applicationId}.provider", file)
+    }
+
+    private fun processUri(uri: Uri) {
+        val info = ImageUtil.info(uri = uri, context = this)
+        inputFileInfo.value = "name=${uri}" +
+                "\ntype=${info.type}" +
+                "\nwidth*height=${info.width}x${info.height}" +
+                "\nsize=${info.size / 1024} KB"
+
+        val inputStream = contentResolver.openInputStream(uri)
+        inputStream?.let {
+            val bitmap =
+                BitmapFactory.decodeStream(inputStream, null, BitmapFactory.Options())
+            inputBitmap.value = bitmap
+        }
+
+        inputStream?.close()
+
+        Compressor.run(
+            uri = uri,
+            context = this,
+            quality = 50,
+            outputDir = getOutputDir(),
+            maxWidth = 500,
+            maxHeight = 500,
+        ).onSuccess { output ->
+            outputBitmap.value = BitmapFactory.decodeFile(output.absolutePath)
+
+            val info = ImageUtil.info(output)
+            outputFileInfo.value = "name=${output.name}" +
+                    "\ntype=${info.type}" +
+                    "\nwidth*height=${info.width}x${info.height}" +
+                    "\nsize=${info.size / 1024} KB"
+        }.onFailure {
+            Log.e(TAG, "error=${it.message}")
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
 
-        val outputDir = cacheDir.resolve("output")
-
-        // copy assets res to local
-        AssetsUtils.copyAssetsToCache(this)?.listFiles()?.filter { !it.isDirectory }
-            ?.let { queue.addAll(it) }
-
-        val pickMedia =
+        // pick media launcher
+        val pickMediaLauncher =
             registerForActivityResult(ActivityResultContracts.PickVisualMedia()) { uri ->
                 Log.e(TAG, "Pick Media: $uri")
                 mediaUri = uri
                 uri?.let {
-
-                    val info = ImageUtil.info(uri = uri, context = this)
-                    inputFileInfo.value = "name=${uri}" +
-                            "\ntype=${info.type}" +
-                            "\nwidth*height=${info.width}x${info.height}" +
-                            "\nsize=${info.size / 1024} KB"
-
-                    val inputStream = contentResolver.openInputStream(uri)
-                    inputStream?.let {
-                        val bitmap =
-                            BitmapFactory.decodeStream(inputStream, null, BitmapFactory.Options())
-                        inputBitmap.value = bitmap
-                    }
-
-                    inputStream?.close()
-
-                    Compressor.run(
-                        uri = uri, context = this, quality = 50, outputDir = outputDir,
-                        maxWidth = 500,
-                        maxHeight = 500,
-                    )
-                        .onSuccess { output ->
-                            outputBitmap.value = BitmapFactory.decodeFile(output.absolutePath)
-
-                            val info = ImageUtil.info(output)
-                            outputFileInfo.value = "name=${output.name}" +
-                                    "\ntype=${info.type}" +
-                                    "\nwidth*height=${info.width}x${info.height}" +
-                                    "\nsize=${info.size / 1024} KB"
-                        }.onFailure {
-                            Log.e(TAG, "error=${it.message}")
-                        }
+                    processUri(uri)
                 }
+            }
 
+
+        // take picture launcher
+        val takePictureLauncher =
+            registerForActivityResult(ActivityResultContracts.TakePicture()) { result ->
+                if (result) {
+                    pictureUri?.let {
+                        processUri(it)
+                    }
+                }
             }
 
         setContent {
@@ -110,50 +129,21 @@ class MainActivity : ComponentActivity() {
                         inputFileInfo = inputFileInfo.value,
                         compressedBitmap = outputBitmap.value,
                         outputFileInfo = outputFileInfo.value,
-                        onNextClick = {
-                            val file = queue.poll() ?: return@ImageCompressScreen
-
-                            currentFile = file
-                            val info = ImageUtil.info(file)
-                            if (info.width <= 0 || info.height <= 0) {
-                                inputFileInfo.value = "get image info error, file=${file.name}"
-                                return@ImageCompressScreen
-                            }
-
-                            inputBitmap.value = BitmapFactory.decodeFile(file.absolutePath)
-                            inputFileInfo.value = "name=${file.name}" +
-                                    "\ntype=${info.type}" +
-                                    "\nwidth*height=${info.width}x${info.height}" +
-                                    "\nsize=${info.size / 1024} KB"
-                        },
-                        onCompressClick = {
-                            val input = currentFile ?: return@ImageCompressScreen
-
-                            Compressor.run(
-                                file = input, outputDir = outputDir, quality = 50,
-                                maxWidth = 500,
-                                maxHeight = 500,
-                            ).onSuccess { output ->
-                                outputBitmap.value =
-                                    BitmapFactory.decodeFile(output.absolutePath)
-
-                                val info = ImageUtil.info(output)
-                                outputFileInfo.value = "name=${output.name}" +
-                                        "\ntype=${info.type}" +
-                                        "\nwidth*height=${info.width}x${info.height}" +
-                                        "\nsize=${info.size / 1024} KB"
-                            }.onFailure {
-                                Log.e(TAG, "error=${it.message}")
-                            }
-                        },
                         onAlbumClick = {
-                            pickMedia.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+                            pickMediaLauncher.launch(PickVisualMediaRequest(ActivityResultContracts.PickVisualMedia.ImageOnly))
+                        },
+                        onTakePictureClick = {
+                            val file = File(cacheDir, "${UUID.randomUUID()}.jpg")
+                            val uri = getUriForFile(file)
+                            pictureUri = uri
+                            takePictureLauncher.launch(uri)
                         }
                     )
                 }
             }
         }
     }
+
 }
 
 @OptIn(ExperimentalLayoutApi::class)
@@ -164,15 +154,22 @@ fun ImageCompressScreen(
     inputFileInfo: String,
     compressedBitmap: Bitmap?,
     outputFileInfo: String,
-    onNextClick: () -> Unit = {},
-    onCompressClick: () -> Unit = {},
     onAlbumClick: () -> Unit = {},
+    onTakePictureClick: () -> Unit = {},
 ) {
-    Column(modifier = modifier) {
-        FlowRow {
-            Button(onClick = { onNextClick.invoke() }) { Text(text = "Next") }
-            Button(onClick = { onCompressClick.invoke() }) { Text(text = "Compress") }
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    ) {
+        FlowRow(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp),
+            horizontalArrangement = Arrangement.spacedBy(12.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
             Button(onClick = { onAlbumClick.invoke() }) { Text(text = "Album") }
+            Button(onClick = { onTakePictureClick.invoke() }) { Text(text = "TakePicture") }
         }
 
         Row {
